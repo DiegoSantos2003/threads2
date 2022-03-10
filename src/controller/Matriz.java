bed6290291312eea73f9286700756d7de61ab157
Diego Santos <Diego Santos@Diego-PC>
2022-03-09 21:18:57 -0300

    Threads2

---
+++ src/controller/Matriz.java
@@ -0,0 +1,16 @@
+package controller;
+public class Matriz extends Thread {
+	private int linha;
+	private int[] vetor;
+	
+	public MatrizThread(int linha, int[] vetor) {
+		this.linha = linha;
+		this.vetor = vetor;
+	}
+	public void run() {
+		 int soma = 0;
+		 for(int i=0; i < vetor.length; i++) 
+			 soma += vetor[i];
+		 System.out.println("Linha " + linha + " - Soma: " + soma);
+	}
+}
